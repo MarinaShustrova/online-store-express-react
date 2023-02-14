ceec8831b40269e81818b241f9e17d6144f98048
MarinaShustrova <averc.marina@yandex.ru>
2023-02-14 19:16:59 +0300

    registration done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,31 @@
 /* eslint-disable consistent-return */
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const ApiError = require('../error/ApiError');
-
+const { User, Basket } = require('../models/models');
 //* class используется для группировки
-class UserController {
-  async registration(req, res) {
 
+const generateJwt = (id, email, role) => jwt.sign(
+  { id, email, role },
+  process.env.SECRET_KEY,
+  { expiresIn: '24h' },
+);
+class UserController {
+  // async registration(req, res) {
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest('Incorrect email or password!'));
+    }
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiError.badRequest('User with such email alredy exist!'));
+    }
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
   }
 
   async login(req, res) {
