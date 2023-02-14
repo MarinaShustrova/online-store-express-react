fcbeea3c990e705ff278185e1f9fa86203fb1ef6
MarinaShustrova <averc.marina@yandex.ru>
2023-02-14 18:31:55 +0300

    controllers for device type and brand done

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,15 +1,70 @@
 //* class используется для группировки
-class DeviceController {
-  async create(req, res) {
+const uuid = require('uuid');
+const path = require('path');
+const { Device, DeviceInfo } = require('../models/models');
+const ApiError = require('../error/ApiError');
 
+class DeviceController {
+  async create(req, res, next) {
+    try {
+      const {
+        name, price, brandId, info,
+      } = req.body;
+      const { img } = req.files;
+      const fileName = `${uuid.v4()}.jpg`;
+      img.mv(path.resolve(__dirname, '..', 'static', fileName));
+
+      if (info) {
+        info = JSON.parce(info);
+        info.forEach((i) => DeviceInfo.create({
+          title: i.title,
+          description: i.description,
+          deviceId: device.id,
+        }));
+      }
+
+      const device = await Device.create({
+        name, price, brandId, info, img: fileName,
+      });
+
+      return res.json(device);
+    } catch (error) {
+      next(ApiError.badRequest(error.message));
+    }
   }
 
   async getAll(req, res) {
-
+    const {
+      brandId, typeId, limit, page,
+    } = req.query;
+    page = page || 1;
+    limit = limit || 10;
+    const offset = page * limit - limit;
+    let devices;
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll({ limit, offset });
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({ where: { brandId }, limit, offset });
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({ where: { typeId }, limit, offset });
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({ where: { typeId, brandId }, limit, offset });
+    }
+    return res.json(devices);
   }
 
   async getOne(req, res) {
-
+    const { id } = req.params;
+    const device = await Device.findOne(
+      {
+        where: { id },
+        include: [{ model: DeviceInfo, as: 'info' }],
+      },
+    );
+    return res.json(device);
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -2,20 +2,23 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const sequelize = require('./db');
 const models = require('./models/models');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 
-//* это должно быть в конце 
+//* это должно быть в конце
 app.use(errorHandler);
 
 const start = async () => {
